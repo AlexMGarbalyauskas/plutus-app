5a5e10b1c36d2215e26d833cdf099dbb3f40c61b
EskandarAtrakchi <isgan151@gmail.com>
2024-06-01 16:56:27 +0100

    updated tokens to avax

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import { useState } from "react";
 import "./App.css";
-import React from "react";
-import Register from "./pages/user/Register";
-import Swap from "./pages/Swap";
+import { Link } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
+
+
   return (
     <>
+    <Link to="/user/pay"> 
+      <button>User</button>
+      <button>Seller</button>
+    </Link>
 
-<div className="App">
-      <Header connect={connect} isConnected={isConnected} address={address} />
-      <div className="mainWindow">
-        <Routes>
-          <Route path="/" element={<Swap isConnected={isConnected} address={address} />} />
-          <Route path="/Signup" element={<Signup />} />
-        </Routes>
-      </div>
-
-    </div>
+      <Link to="/user/register">
+        <button>Register</button>
+      </Link>
     </>
   );
 }
