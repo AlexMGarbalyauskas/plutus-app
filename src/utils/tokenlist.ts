d8ff9286deaa6b695f2c8dbe4cb16028d870f7a9
EskandarAtrakchi <isgan151@gmail.com>
2024-06-01 19:27:56 +0100

    token addresses confirmed

--- src/utils/tokenlist.ts
+++ src/utils/tokenlist.ts
@@ -1,66 +1,45 @@
 export const TOKEN_LIST = [
   {
-    ticker: "LINK",
-    img: "https://cdn.moralis.io/eth/0x514910771af9ca656af840dff83e8264ecf986ca.png",
-    name: "Chainlink",
-    address: "0x5947BB275c521040051D82396192181b413227A3",
-    decimals: 18,
+    "ticker": "AVAX",
+    "img": "https://cdn.routescan.io/_next/image?url=https%3A%2F%2Fcms-cdn.avascan.com%2Fcms2%2Favax_32.c6f54294f728.png&w=16&q=100",
+    "name": "Avalanche",
+    "address": "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
+    "decimals": 18
   },
   {
-    ticker: "WETH",
-    img: "https://cdn.moralis.io/eth/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png",
-    name: "Wrapped Ethereum",
-    address: "0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB",
-    decimals: 18,
+    "ticker": "LINK",
+    "img": "https://cdn.moralis.io/eth/0x514910771af9ca656af840dff83e8264ecf986ca.png",
+    "name": "Chainlink",
+    "address": "0x5947BB275c521040051D82396192181b413227A3",
+    "decimals": 18
   },
   {
-    ticker: "WBTC",
-    img: "https://cdn.moralis.io/eth/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png",
-    name: "Wrapped Bitcoin",
-    address: "0x50b7545627a5162F82A992c33b87aDc75187B218",
-    decimals: 8,
+    "ticker": "WETH",
+    "img": "https://cdn.moralis.io/eth/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png",
+    "name": "Wrapped Ethereum",
+    "address": "0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB",
+    "decimals": 18
   },
   {
-    ticker: "MATIC",
-    img: "https://cdn.moralis.io/eth/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png",
-    name: "Matic Token",
-    address: "0x145ad28a42bf334104610f7836d0945dffb6de63",
-    decimals: 18,
+    "ticker": "WBTC",
+    "img": "https://cdn.moralis.io/eth/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png",
+    "name": "Wrapped Bitcoin",
+    "address": "0x50b7545627a5162F82A992c33b87aDc75187B218",
+    "decimals": 8
   },
   {
-    ticker: "UNI",
-    img: "https://cdn.moralis.io/eth/0x1f9840a85d5af5bf1d1762f925bdaddc4201f984.png",
-    name: "Uniswap",
-    address: "0x0b3F868E0BE5597D5DB7fEB59E1CADBb0fdDa50a",
-    decimals: 18,
+    "ticker": "CRV",
+    "img": "https://cdn.moralis.io/eth/0xd533a949740bb3306d119cc777fa900ba034cd52.png",
+    "name": "Curve DAO Token",
+    "address": "0x249848BeCA43aC405b8102Ec90Dd5F22CA513c06",
+    "decimals": 18
   },
   {
-    ticker: "CRV",
-    img: "https://cdn.moralis.io/eth/0xd533a949740bb3306d119cc777fa900ba034cd52.png",
-    name: "Curve DAO Token",
-    address: "0x249848BeCA43aC405b8102Ec90Dd5F22CA513c06",
-    decimals: 18,
-  },
-  {
-    ticker: "MKR",
-    img: "https://cdn.moralis.io/eth/0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2.png",
-    name: "Maker",
-    address: "0x2eE014d05970E001Fc30Fe68bD1D066c0D274892",
-    decimals: 18,
-  },
-  {
-    ticker: "SHIB",
-    img: "https://cdn.moralis.io/eth/0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce.png",
-    name: "Shiba Inu",
-    address: "0x8f2654E28c1569115a6E871d4A48c7C92B7de3F2",
-    decimals: 18,
-  },
-  {
-    ticker: "AAVE",
-    img: "https://cdn.moralis.io/eth/0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9.png",
-    name: "AAVE",
-    address: "0x63a72806098Bd3D9520cC43356dD78afe5D386D9",
-    decimals: 18,
+    "ticker": "AAVE",
+    "img": "https://cdn.moralis.io/eth/0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9.png",
+    "name": "AAVE",
+    "address": "0x63a72806098Bd3D9520cC43356dD78afe5D386D9",
+    "decimals": 18
   },
 ] as const;
 
